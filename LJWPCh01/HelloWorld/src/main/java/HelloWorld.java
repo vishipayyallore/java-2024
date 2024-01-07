f5a0f94e1bad12d114bc349e6c8f3f3dadbaa88c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-07 21:37:59 +0530

    Update HelloWorld.java

--- LJWPCh01/HelloWorld/src/main/java/HelloWorld.java
+++ LJWPCh01/HelloWorld/src/main/java/HelloWorld.java
@@ -21,6 +21,14 @@ public class HelloWorld {
         String closingHours = "20:00";
         greeting = String.format("We open at %s and close at %s.", openingHours, closingHours);
         displayMessage(greeting);
+
+        String yourName = "Maaike";
+        greeting = String.format("Welcome %s, to Mesozoic Eden!", yourName);
+        displayMessage(greeting);
+        greeting = "Mesozoic Eden is safe and secure.";
+        displayMessage(greeting);
+
+        showEntrance();
     }
 
     private static int doubleNumber(int input) {
@@ -29,5 +37,19 @@ public class HelloWorld {
 
     private static void displayMessage(String message){
         System.out.println(message);
+    }
+
+    private static void showEntrance(){
+        String greeting = "***** Welcome to Mesozoic Eden *****";
+        displayMessage(greeting);
+
+        String openingHours = "08:00";
+        String closingHours = "20:00";
+        greeting = String.format("We open at %s and close at %s.", openingHours, closingHours);
+        displayMessage(greeting);
+
+        greeting = "Mesozoic Eden is safe and secure.";
+        displayMessage(greeting);
+
     }
 }
