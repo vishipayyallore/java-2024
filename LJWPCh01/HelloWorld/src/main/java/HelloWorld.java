6b1e0311364c0f61c754a71631bcbe004b827f1e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-07 21:28:23 +0530

    Update HelloWorld.java

--- LJWPCh01/HelloWorld/src/main/java/HelloWorld.java
+++ LJWPCh01/HelloWorld/src/main/java/HelloWorld.java
@@ -6,10 +6,23 @@ public class HelloWorld {
         String greeting = "Hello Java 21 world!";
         System.out.println(greeting);
 
-        System.out.println("The doubled number is: " + doubleNumber(5));
+        int value = 5;
+        System.out.println("The doubled number is: " + doubleNumber(value));
+
+        greeting = "Welcome to Mesozoic Eden";
+        displayMessage(greeting);
+
+        String name = "Maaike";
+        String position = "Park Manager";
+        greeting = String.format("My name is %s and I want to be a %s in Mesozoic Eden.", name, position);
+        displayMessage(greeting);
     }
 
-    public static int doubleNumber(int input) {
+    private static int doubleNumber(int input) {
         return input * 2;
+    }
+
+    private static void displayMessage(String message){
+        System.out.println(message);
     }
 }
